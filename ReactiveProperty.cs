3b331a076b9ff9379c0d9ede7dc9d8bd4090dae7
Alexander V <ax.voronkov@gmail.com>
2020-07-28 19:22:05 +0300

    Add ReactiveProperty.cs & Singleton.cs

---
+++ ReactiveProperty.cs
@@ -0,0 +1,27 @@
+﻿public class ReactiveProperty<T>
+{
+    private T _value = default;
+    public T Value
+    {
+        get {
+            return _value;
+        }
+        set {
+            _value = value;
+            OnChanged?.Invoke();
+        }
+    }
+
+    public delegate void OnChange();
+    public event OnChange OnChanged;
+
+    public ReactiveProperty(T value)
+    {
+        _value = value;
+    }
+
+    public void SilentSet(T value)
+    {
+        _value = value;
+    }
+}
